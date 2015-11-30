d3b91566858574f1650774c5609e5b435111977b
Andy Foy <andy@andyfoyconsulting.co.uk>
2015-11-30 17:48:30 +0000

    Temporarily remove all .sql files
    Temporarily remove all .sql files to correct treatment as binary files
    by GitHub.

--- SQL Execute SelectSppRecords.sql
+++
@@ -1,5 +0,0 @@
-USE NBNData
-GO
-
-EXECUTE dbo.AFSelectSppRecords 'dbo', 'COMBINEDEXTRACTPOLYGONS', 'PartnerName', 'London Wildlife Trust', 'LWT', 'GiGL_SPP_extract', 'test'
-GO
--- SQL Execute SpatialiseSppExtract.sql
+++
@@ -1,5 +0,0 @@
-USE NBNData
-GO
-
-EXECUTE dbo.AFSpatialiseSppExtract 'dbo', 'GiGL_SPP_Test', 400000, 700000, 50000, 350000, 1, 10000, 0, 1
-GO
